f6ecbc6bc833a4ad9d12fd06e505628fee6dd3ee
armj505 <ahmad.almohanna@outlook.com>
2023-09-17 20:21:21 +0300

    task completed + spicy

--- index.js
+++ index.js
@@ -39,3 +39,74 @@ const q5 = numbers.filter((x) => x >= 20).map((x) => x ** 2);
 const q6 = numbers.filter((x) => x % 5 === 0).map((x) => x * 3);
 console.log(q5);
 console.log(q6);
+
+// EXTRA
+
+/*****************************************************************
+Part 4: Extra not required 🌶️🌶️🌶️
+******************************************************************/
+/*************************************
+ * logger(array)
+ *
+ * - Accepts an array
+ * - Logs every element of the array
+ ************************************/
+const logger = function (array) {
+  array.forEach((x) => {
+    console.log(x);
+  });
+};
+logger([1, 2, 3, 4]);
+
+/*************************************
+ * toCelsius(temperatures)
+ *
+ * - Accepts an array of temperatures
+ *   in degrees Fahrenheit
+ * - Returns an array of temperatures
+ *   in degrees Celsius
+ *
+ * The conversion is:
+ *   C = (F - 32) * (5/9)
+ ************************************/
+const toCelsius = function (temperatures) {
+  const fehArray = temperatures.map((x) => Math.round((x - 32) * (5 / 9)));
+  console.log(fehArray);
+};
+toCelsius([85, 90, 110]);
+
+/**************************************
+ * hottestDays(temperatures, threshhold)
+ *
+ * - Accepts an array of temperatures
+ * - Accepts a threshhold temperature
+ * - Returns an array of temperatures
+ *   that exceed the threshhold
+ ***************************************/
+const hottestDays = function (temperatures, threshhold) {
+  const hotDays = temperatures.filter((x) => x > threshhold);
+  console.log(hotDays);
+};
+hottestDays([10, 20, 30], 25);
+
+/******************************************
+ * logHottestDays(temperatures, threshhold)
+ *
+ * - Accepts an array of temperatures
+ *   IN DEGREES FAHRENHEINT
+ * - Accepts a threshhold temperature
+ *   IN DEGREES FAHRENHEINT
+ * - Logs temperatures that exceed the
+ *   threshhold to the console
+ *   IN DEGREES CELSIUS
+ *
+ * hint: you can combine
+ *       all previous functions
+ *******************************************/
+const logHottestDays = function (temperatures, threshhold) {
+  const hottestDay = temperatures
+    .filter((x) => x > threshhold)
+    .map((x) => Math.round((x - 32) * (5 / 9)));
+  console.log(hottestDay);
+};
+logHottestDays([80, 90, 110], 85);
